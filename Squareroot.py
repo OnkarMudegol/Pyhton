8c461d2243234665356da017a0d1842a371082e0
OnkarMudegol <omudegol@gmail.com>
2022-10-01 19:44:38 +0530

    Area by Herons Formula

---
+++ Squareroot.py
@@ -0,0 +1,5 @@
+#Find the square root of a no.
+#Use Operators option
+x=input("Type a number")
+y=int(x)**0.5
+print("The squareroot is", y)
--- areaOfTriangle.py
+++ areaOfTriangle.py
@@ -1,2 +1,8 @@
 #find area of triangle
 #Herons Formula
+x=input("Enter the first vertice of Triangle")
+y=input("Enter the second vertice of Triangle")
+z=input("Enter the third vertice of Triangle")
+S=int(x)/2+int(y)/2+int(z)/2
+area=(S*(S-int(x))*(S-int(y))*(S-int(z)))**0.5
+print("The area is",area) 
