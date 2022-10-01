738e7c348bbe0fd44d60d6c18fb73e9ed7ef5fa6
OnkarMudegol <omudegol@gmail.com>
2022-10-01 16:26:02 +0530

    Swap Function

---
+++ Swap.py
@@ -0,0 +1,12 @@
+#Take imputs from user for two numbers (x and y)
+from ast import Assign
+
+
+x=input("Enter first value")
+y=input("Enter second number")
+z=int
+z=x
+x=y
+y=z 
+print("First No. is", x)
+print("Second No. is", y)
