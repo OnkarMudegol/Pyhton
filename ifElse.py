8651b5a95ef02b00be082d1148f6dfa23fb5c156
OnkarMudegol <omudegol@gmail.com>
2022-10-09 13:09:08 +0530

    Conditional function

---
+++ ifElse.py
@@ -0,0 +1,11 @@
+#Take user input for x and y
+#If x is less than y print YAYY
+#else print NAYY
+x=float(input("Enter first value:"))
+y=float(input("Enter second value:"))
+
+if x < y:
+    print("YAYY")
+
+else:
+    print("NAYY")
