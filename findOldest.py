c449b9a7570f47254467c6a5bc609a86459d73f3
OnkarMudegol <omudegol@gmail.com>
2022-10-09 15:41:06 +0530

    Three people age comparison.

---
+++ findOldest.py
@@ -0,0 +1,13 @@
+#take input of age from three people 
+#dtermine the oldest and youngest
+age1=float(input("Enter first person's age:"))
+age2=float(input("Enter second person's age:"))
+age3=float(input("Enter third person's age:"))
+if age1 > age2 and age1 > age3:print("The oldest age is:", age1)
+elif age2 > age1 and age2 > age3:print("The oldest age is:", age2)
+elif age3 > age1 and age3 > age2:print("The oldest age is:", age3)
+
+if age1 < age2 and age1 < age3:print("The youngest age is:", age1)
+elif age2 < age1 and age2 < age3:print("The youngest age is:", age2)
+elif age3 < age1 and age3 < age2:print("The youngest age is:", age3)
+else:print("All are of same age.")
