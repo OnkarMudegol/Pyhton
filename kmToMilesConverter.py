3340129c0cced2aa36f11ad07096d3e682a25aee
OnkarMudegol <omudegol@gmail.com>
2022-10-09 12:49:42 +0530

    Converter program

---
+++ kmToMilesConverter.py
@@ -0,0 +1,7 @@
+#Take user input for Km and display output in miles
+from tkinter import Y
+
+
+distaceInKm=float(input("Enter distance in Km:"))
+distanceInMiles=distaceInKm*0.62137119
+print("The distance in miles is:",distanceInMiles)
---
+++ milesToKmConverter.py
@@ -0,0 +1,4 @@
+#Input in miles, output in km
+distanceInMiles=float(input("Enter the distance in miles:"))
+distanceInKm=(distanceInMiles/0.62137119)
+print("The distance in kms is:",distanceInKm)
