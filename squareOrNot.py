09c2f348cea952866b658cf87e523a453031eab9
OnkarMudegol <omudegol@gmail.com>
2022-10-09 13:15:43 +0530

    Square detection

---
+++ squareOrNot.py
@@ -0,0 +1,10 @@
+#take user input for values of length and breadth 
+#print if square or not
+length=float(input("Enter the length:"))
+breadth=float(input("Enter the breadth:"))
+
+if length == breadth:    #if length and bredth are equal then it is a square, if not then not a square
+    print("It is a Square.")
+
+else: 
+    print("Not a Square.")
