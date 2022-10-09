2566a148c0f9c0c627343527e65afd9ba38c7f1d
OnkarMudegol <omudegol@gmail.com>
2022-10-09 13:43:46 +0530

    Exam attendence percentage

--- attendencePercentage.py
+++ attendencePercentage.py
@@ -1,7 +1,1 @@
-#calculate the attendence percentage of sem and tell if eligible for exam
-classesAttended = float(input("How many clases have you attended?"))
-attendencePercentage= (classesAttended/125)*100
-if attendencePercentage > 75:
-    print("Your attendence percentage is:", attendencePercentage, ",you are eligible for exams.")
 
-else: print("Your attendence percentage is:", attendencePercentage, ",you are not eligible for exams.")
