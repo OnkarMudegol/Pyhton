05d4d79df447f96111b5884516eaaeaac1ba3bea
OnkarMudegol <omudegol@gmail.com>
2022-10-09 16:08:16 +0530

    b

--- greatesNumber.py
+++
@@ -1,7 +0,0 @@
-#Take two numbers form user (int/float)
-#print the greatest number
-num1=float(input("Enter first number:"))
-num2=float(input("Enter second number:"))
-if num1 > num2:print("The greates no. is", num1)
-elif num2 > num1:print("The greates no. is", num2)
-else:print("Both number are equal.")
