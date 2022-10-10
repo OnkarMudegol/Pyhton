2ef369855bcd01db023c0c227e6811de5b68fb90
OnkarMudegol <omudegol@gmail.com>
2022-10-10 18:21:33 +0530

    Sum of first N numbers

---
+++ sumOfFirstnSquares.py
@@ -0,0 +1,5 @@
+#to calculate sum of squares of first n numbers as output
+#n is user input
+n = float(input("Enter a value for n: "))
+sum = (n*(n+1)*(2*n+1))/6
+print("The sum of squares of first n numbers is:",sum)
