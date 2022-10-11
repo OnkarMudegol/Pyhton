1d03c2b0114af4a9be880c2c870966354922a6c0
OnkarMudegol <omudegol@gmail.com>
2022-10-11 22:49:56 +0530

    Temp concerter (Celcius to Farhenite and Kelvin)

---
+++ celciusToFarheniteAndKelvin.py
@@ -0,0 +1,8 @@
+#to take input as celcius to farhenite
+#take celcius as input and print farhenite as output
+#take celcius input from user
+celcius = float(input("Enter Celcius value:"))
+farhenite = celcius*1.8 +32
+kelvin = celcius+273.15
+print("The temp in farhenite is:", farhenite)
+print("The temp in kelvin is:", kelvin)
