0a02175af4dafb449cd83c18ee4775c77a75076d
OnkarMudegol <omudegol@gmail.com>
2022-10-12 21:55:49 +0530

    Program to calculate Area and perimeter of a rectangle

---
+++ areaAndPerimeterOfRectangle.py
@@ -0,0 +1,8 @@
+#to take length and width of rectangle as input and
+#and print area and perimeter as output
+length = float(input("Enter the length of rectangle:"))
+breadth = float(input("Enter the breadth of rectangle:"))
+area = length*breadth
+perimeter = (length+breadth)*2
+print("The area of rectangle is:",area)
+print("The perimeter of the rectangle is",perimeter)
