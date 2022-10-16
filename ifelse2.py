422c44f0acab445342a6ba009dafe5dbe66724dc
OnkarMudegol <omudegol@gmail.com>
2022-10-16 12:12:21 +0530

    If else question

---
+++ ifelse2.py
@@ -0,0 +1,8 @@
+#take a user input 
+#check whether the input number is divisible by 2 and 3 both
+number = int(input(""))
+if number%2==0 and number%3==0:
+    print("True")
+else:print("False")
+
+    
