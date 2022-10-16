66571173f4e1b98d2f56417f301e290d36791d5d
OnkarMudegol <omudegol@gmail.com>
2022-10-16 13:49:19 +0530

    About array, lists and different functions reated to it

---
+++ array.py
@@ -0,0 +1,18 @@
+#introduction to array 
+a = "HELLO WORLD"
+print(a[6])
+print(a[3])
+print(a[-1])
+print(a[0])
+print(a[-11])
+print(len(a))
+sentence = "A quick brown fox jumps over a lazy dog"
+if "fox" in sentence:
+    print("Yes it is there")
+else: print("No")
+if "ox" in sentence:
+    print("Yes it is there")
+else: print("No")
+if "ox" not in sentence:
+    print("Yes it is there")
+else: print("No")
