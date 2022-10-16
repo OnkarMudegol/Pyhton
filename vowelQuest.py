c45352d28777d24de2b12b1ebe4d000076510666
OnkarMudegol <omudegol@gmail.com>
2022-10-16 15:35:17 +0530

    Vowel Question

---
+++ vowelQuest.py
@@ -0,0 +1,7 @@
+#take vowel input from user
+#print if input is vowel or not
+vowel = str(("AEIOUaeiou"))
+alphabet = str(input("Enter a vowel:"))
+if alphabet in vowel:
+    print("You are correct")
+else:print("Incorrect")
