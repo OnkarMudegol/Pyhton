7cefb5a109459087272e9324548be9d3956e7c1b
OnkarMudegol <omudegol@gmail.com>
2022-10-16 15:29:12 +0530

    Different Indexes

---
+++ index.py
@@ -0,0 +1,8 @@
+sentence = "A quick brown fox jumps over lazy dog"
+print(sentence[2:17])
+print(sentence[2:])
+print(sentence[:8])
+print(sentence[:])
+sentence2 = "Hello World"
+print(sentence2[-5:-2])
+
