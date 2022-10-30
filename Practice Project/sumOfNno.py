a986eed10c42369425d8631b446e79601246509e
OnkarMudegol <omudegol@gmail.com>
2022-10-30 16:08:29 +0530

    Using for loop to find sum of N numbers

---
+++ Practice Project/sumOfNno.py
@@ -0,0 +1,14 @@
+#write a program to accept a number from a user
+#calculate the sum of all the numberas from
+#1 to given number
+#eg user inpur 10
+#output should be 55
+#1+2+3+4+5+6+7+8+9+10
+i =  int(input("Enter a number:"))
+sum = 0
+for i in range (1,i+1):
+    sum = sum + i
+print(sum)
+
+
+    
