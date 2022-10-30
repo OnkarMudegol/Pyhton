dd8326cd657b30a0ade9d9d78539c531533c0122
OnkarMudegol <omudegol@gmail.com>
2022-10-30 16:31:35 +0530

    Printing a list in reverse order using for loop

---
+++ Practice Project/listReverseForLoop.py
@@ -0,0 +1,6 @@
+# numbers=[12, 75, 150, 180, 145]
+#using for loop print the above list in reverse order
+numbers=["12", "75", "150", "180", "145"]
+i = len(numbers)
+for i in range(i-1,-1,-1):
+    print(numbers[i])
