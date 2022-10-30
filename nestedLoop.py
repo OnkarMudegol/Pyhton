ef426a994d6c17e6b0910ee488c827e7e2d6a71f
OnkarMudegol <omudegol@gmail.com>
2022-10-30 16:08:13 +0530

    Using nested loops

---
+++ nestedLoop.py
@@ -0,0 +1,7 @@
+#nested loops
+color = ["Red", "Blue", "Green", "Black", "White"]
+cars = ["Defender", "GTR", "G-Wagon", "Koenisegg", "Urus"]
+
+for i in color:
+    for j in cars:
+        print(i,j)
