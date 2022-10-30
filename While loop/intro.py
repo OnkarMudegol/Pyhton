e2fa96ecfee52032248172457ffe2ba862f158a1
OnkarMudegol <omudegol@gmail.com>
2022-10-30 13:42:57 +0530

    Intro to while loop

---
+++ While loop/intro.py
@@ -0,0 +1,15 @@
+#intro to while loop
+#print 1-10 using while loop
+i = 0
+while i < 10:
+  i = i+1
+  if i == 5:
+    continue
+  print(i)
+else:
+    print("I is greater than 10")
+
+    
+    
+     
+
