ed5b9e17be62b0f4098b36e9467a1ef237e31dd1
OnkarMudegol <omudegol@gmail.com>
2022-11-06 13:29:52 +0530

    Creating a defination for any no of arguments

---
+++ Functions/anyNoOfArgument.py
@@ -0,0 +1,7 @@
+#Create a function in such a way that we can pass any number of arguments
+# to this and it should display each argument's vaue
+def st(*arg):
+    for i in set:
+        print(i)
+set=("1","2","3","4","5","6","7","8","9","10")
+st(set)
