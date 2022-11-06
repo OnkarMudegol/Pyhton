6ac122cedec19e7ac37761a305b013c5b75cacaf
OnkarMudegol <omudegol@gmail.com>
2022-11-06 13:07:39 +0530

    Name and age input function

---
+++ Functions/nameAndAge.py
@@ -0,0 +1,8 @@
+#create a function that will take two arguments, name and age and print
+# their values
+name = input("Enter your name:")
+age = input("Enter your age:")
+def st(name,age):
+    st=(name+" "+age)
+    print(st)
+st(name,age)
