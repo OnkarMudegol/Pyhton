39e1eb519170a1295749af5e47f3a9f42a14b5ec
OnkarMudegol <omudegol@gmail.com>
2022-11-06 13:55:31 +0530

    Loops

---
+++ Loops/fibonaciSeries.py
@@ -0,0 +1,6 @@
+#take two numbers from user and print fibonaci series till 10 times
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+series = ()
+next_num = ""
+for i 
