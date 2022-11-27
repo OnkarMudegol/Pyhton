6591a76ba90095e66bb608074311042b43ba0112
OnkarMudegol <omudegol@gmail.com>
2022-11-27 13:30:03 +0530

    Introduction to tupple

--- tupple/intro.py
+++ tupple/intro.py
@@ -1,10 +1,12 @@
-tuple1 = ("apple","grapes")             #intro to tuples
+tuple1 = ("apple","grapes")              #intro to tuples
 print(tuple1)
-tuple2 = ("car", "bike", "boat", "jet")   #printing indexes
-# print(tuple2[2])
-list1 = list(tuple2)                      #changing tuple to lists
+tuple2 = ("car", "bike", "boat", "jet")  #printing indexes
+print(tuple2[2])
+list1 = list(tuple2)                     #changing tuple to lists
 list1.append("cycle")
 tuple3 = tuple(list1)
 print(tuple3)
-tuple4 = (1,2,3,[4,5,6],7,8)             #lists inside a tuple
+tuple4 = (1,2,3,[4,5,6],7,8)               #lists inside a tuple
 print(tuple4[3][1])                      #ascessing elements inside list
+tuple4[3][2]=8                             
+print(tuple4)
