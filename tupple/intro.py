9323efb06201e3e60a12c4e9027afd0b92f3aa7f
OnkarMudegol <omudegol@gmail.com>
2022-11-27 13:40:20 +0530

    Intro to tupple

--- tupple/intro.py
+++ tupple/intro.py
@@ -1,19 +1,19 @@
-tuple1 = ("apple","grapes")              #intro to tuples
-print(tuple1)
-tuple2 = ("car", "bike", "boat", "jet")  #printing indexes
-print(tuple2[2])
-list1 = list(tuple2)                     #changing tuple to lists
-list1.append("cycle")
-tuple3 = tuple(list1)
-print(tuple3)
-tuple4 = (1,2,3,[4,5,6],7,8)               #lists inside a tuple
-print(tuple4[3][1])                      #ascessing elements inside list
-tuple4[3][2]=8                             
-print(tuple4)
-tuple5 = (30,40)
-tuple6 = (10,20,30)
-temp= tuple5
-tuple5 = tuple6
-tuple6 = temp
-print(tuple5)
-print(tuple6)
+# tuple1 = ("apple","grapes")              #intro to tuples
+# print(tuple1)
+# tuple2 = ("car", "bike", "boat", "jet")  #printing indexes
+# print(tuple2[2])
+# list1 = list(tuple2)                     #changing tuple to lists
+# list1.append("cycle")
+# tuple3 = tuple(list1)
+# print(tuple3)
+# tuple4 = (1,2,3,[4,5,6],7,8)               #lists inside a tuple
+# print(tuple4[3][1])                      #ascessing elements inside list
+# tuple4[3][2]=8                             
+# print(tuple4)
+# tuple5 = (30,40)                         #swapping tupple
+# tuple6 = (10,20,30)
+# temp= tuple5
+# tuple5 = tuple6
+# tuple6 = temp
+# print(tuple5)
+# print(tuple6)
