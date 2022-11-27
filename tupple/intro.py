84ca8b8facf8b11673927c2249759aa5014453e1
OnkarMudegol <omudegol@gmail.com>
2022-11-27 13:23:36 +0530

    Intorduction to tupple

--- tupple/intro.py
+++ tupple/intro.py
@@ -6,3 +6,5 @@ list1 = list(tuple2)                      #changing tuple to lists
 list1.append("cycle")
 tuple3 = tuple(list1)
 print(tuple3)
+tuple4 = (1,2,3,[4,5,6],7,8)             #lists inside a tuple
+print(tuple4[3][1])                      #ascessing elements inside list
