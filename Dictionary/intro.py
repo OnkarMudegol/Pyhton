ef5a601197cef1b3fec1dc158bec46a37c4bc10f
OnkarMudegol <omudegol@gmail.com>
2022-12-04 13:17:03 +0530

    Basic Dictionary properties

---
+++ Dictionary/intro.py
@@ -0,0 +1,20 @@
+myname = {
+    "name":"Onkar Mudegol",
+    "age":"18",
+    "Class":"first year",
+    "percentage":"96%" 
+}
+# print(myname)
+# print(myname.keys())
+# print(myname.get("name"))
+# print(len(myname))
+# print(myname.values())
+# print(myname.items())
+# myname["age"]=54
+# print(myname)
+# myname.update({"age":"66"})
+# print(myname)
+# myname.pop("age")
+# print(myname)
+# print("The keys are:",myname.keys())
+# print("The values are:",myname.values())
