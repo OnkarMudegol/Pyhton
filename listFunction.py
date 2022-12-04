8d1438b4cd603bd8ba46cb4c463678d46098f926
OnkarMudegol <omudegol@gmail.com>
2022-12-04 16:30:31 +0530

    List functions

---
+++ listFunction.py
@@ -0,0 +1,17 @@
+#create a function to separate even numbers from a list
+# list1 = [3,2,8,16,11,34,17]
+# output=list(filter(lambda i:i%2==0,list1))
+# print(output)
+# output2=list(filter(lambda i: i>15,list1))
+# print(output2)
+# output3 = list(map(lambda i:i**2,list1))
+# print(output3)
+# from functools import reduce
+# output4=reduce(lambda a,b: a+b,list1)
+# print(output4)
+# list2=[10,20,30,40,50]
+# output5=list(map(lambda i:i*3,list2))
+# print(output5)
+# list3= [34,88,30,22,10,15,18]
+# output5=list(filter(lambda i: i%5==0, list3))
+# print(output5)
