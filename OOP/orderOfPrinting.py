41972f9dd611eb1fd861e5f5b8be1c2ded493971
OnkarMudegol <omudegol@gmail.com>
2022-12-18 16:11:20 +0530

    Order of printing

---
+++ OOP/orderOfPrinting.py
@@ -0,0 +1,25 @@
+class A:
+    def __init__(self):
+        print("This is init of method A")
+
+    def method1(self):
+        print("This is method 1")
+
+class B:
+    def __init__(self):
+        # super().__init__()
+        print("This is init of method B")
+
+    def method2(self):
+        print("This is method  2")
+
+class C(B,A):
+    def __init__(self):
+        super().__init__()
+        print("This is init of method 3")
+
+    def method3(self):
+        print("This is method 3")
+
+objC = C()
+
