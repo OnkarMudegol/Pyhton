793acb5a101956a75a4ab28c365c7e596bc90934
OnkarMudegol <omudegol@gmail.com>
2022-12-18 16:11:38 +0530

    Overloading Methods

---
+++ OOP/overLoading.py
@@ -0,0 +1,16 @@
+#method overloading - when function names are same but number of parameters
+# def add(a,b,c = 0):
+#     return a+b+c
+# print(add(2,3))
+# print(add(2,3,7))
+
+
+# class Rect:
+#     def calculateArea(self,length = None, breadth = None):
+#         if length != None and breadth!=None:
+#             return length*breadth
+#         elif length != None:
+#             return length*length
+# rectangle = Rect()
+# print(rectangle.calculateArea(2,3))
+# print(rectangle.calculateArea(2))
